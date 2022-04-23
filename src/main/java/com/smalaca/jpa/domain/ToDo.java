67531a608d44b468a3ae89706c5c3ef0e3afef50
smalaca <ab13.krakow@gmail.com>
2022-04-23 14:34:04 +0200

    use specific name for table and columns of ToDo entity

--- src/main/java/com/smalaca/jpa/domain/ToDo.java
+++ src/main/java/com/smalaca/jpa/domain/ToDo.java
@@ -7,17 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.PostLoad;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.util.UUID;
 
 @Entity
+@Table(name = "TODOS")
 @ToString
 public class ToDo {
     @Id
     @GeneratedValue
+    @Column(name = "TODO_ID")
     private UUID id;
-    @Column
+
+    @Column(name = "TODO_SUBJECT")
     private String subject;
+
     @Transient
     private String firstSubjectLetter;
 
