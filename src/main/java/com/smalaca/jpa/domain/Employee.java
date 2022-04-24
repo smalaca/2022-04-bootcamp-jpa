d916cb68ca18302c425f9670e0644d50acf03cc9
smalaca <ab13.krakow@gmail.com>
2022-04-24 15:51:24 +0200

    inheritance with SINGLE_TABLE strategy

--- src/main/java/com/smalaca/jpa/domain/Employee.java
+++ src/main/java/com/smalaca/jpa/domain/Employee.java
@@ -1,9 +1,11 @@
 package com.smalaca.jpa.domain;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.time.LocalDate;
 
 @Entity
+@DiscriminatorValue(value = "EMPLOYEE")
 public class Employee extends Person {
     private LocalDate workStartDate;
 }
--- src/main/java/com/smalaca/jpa/domain/Person.java
+++ src/main/java/com/smalaca/jpa/domain/Person.java
@@ -1,6 +1,7 @@
 package com.smalaca.jpa.domain;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,7 +10,8 @@ import javax.persistence.InheritanceType;
 import java.util.UUID;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "PERSON_TYPE")
 public abstract class Person {
     @Id
     @GeneratedValue
